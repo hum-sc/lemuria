2ca23525ed32915e97733e9b36068b34334b6980
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-10 07:31:34 -0600

    inicializando con gradle

---
+++ src/tests/MapaTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import model.Mapa;
+
+
+public class MapaTest {
+    Mapa mapa;
+
+    @Test
+    public void moverDerecha(){
+
+    }
+    public void moverIzquierda(){
+
+    }
+    public void moverArriba(){
+
+    }
+    public void salirDelMapa(){
+
+    }
+
+}
